e9830e8f361e1d2b52358e383c91b972e7a6e3ac
Kk Shinkai <kkshinkai@outlook.com>
2021-02-08 18:31:27 +0800

    Initialize the project

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "shiny-lang",
+    dependencies: [],
+    targets: [
+        .target(name: "Shiny", dependencies: []),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Shiny Programming Language
+
+A tiny self-hosted Swift compiler, for learning the Swift language and the Swift
+compiler. This is an experimental project, do not try to use it in a production
+environment.
