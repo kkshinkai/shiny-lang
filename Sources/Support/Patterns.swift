115d0e11741ef051ac00199da91eeba8c8da2041
Kk Shinkai <kkshinkai@outlook.com>
2021-02-10 01:40:51 +0800

    A little optimization

--- Sources/Support/Patterns.swift
+++ Sources/Support/Patterns.swift
@@ -1,7 +1,3 @@
 public func ~=(pattern: Character, value: UInt8) -> Bool {
-    if let asciiValue = pattern.asciiValue {
-        return asciiValue == value
-    } else {
-        return false
-    }
+    pattern.asciiValue == .some(value)
 }
